1367ca6980882b2d8fcee3629e3a22693310b7d8
SeamanZhang <lion52@163.com>
2017-03-28 09:57:16 +0800

    第10章 Java语言的反射机制

---
+++ src/chapter10/remotecall/HelloService.java
@@ -0,0 +1,16 @@
+package chapter10.remotecall;
+
+import java.util.Date;
+
+public interface HelloService {
+    public String echo(String msg);
+
+    public Date getTime();
+}
+
+
+/****************************************************
+ * 作者：孙卫琴                                     *
+ * 来源：<<Java网络编程精解>>                       *
+ * 技术支持网址：www.javathinker.org                *
+ ***************************************************/
---
+++ src/chapter10/remotecall/HelloServiceImpl.java
@@ -0,0 +1,20 @@
+package chapter10.remotecall;
+
+import java.util.Date;
+
+public class HelloServiceImpl implements HelloService {
+    public String echo(String msg) {
+        return "echo:" + msg;
+    }
+
+    public Date getTime() {
+        return new Date();
+    }
+}
+
+
+/****************************************************
+ * 作者：孙卫琴                                     *
+ * 来源：<<Java网络编程精解>>                       *
+ * 技术支持网址：www.javathinker.org                *
+ ***************************************************/
